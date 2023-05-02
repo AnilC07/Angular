1857b2e5e8b22906e417aebbac93958f7da3f672
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-05-02 22:05:31 +0200

    npm instal

---
+++ section 6 - Course Project - Components and Data Binding/src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+export class Ingredient
+{
+//   public name:string
+//   public amount:number
+
+
+//   constructor(  name:string,   amount:number){
+// this.name = name
+// this.amount = amount
+//   }
+
+// équivaut à 
+
+  constructor( public name:string,  public amount:number){
+
+  }
+}
